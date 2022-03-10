ccbed799b304d9094291555460a19a9888d3b58d
LegionEcarlate <80052613+LegionEcarlate@users.noreply.github.com>
2022-03-10 21:03:37 +0100

    jsp3

--- src/www/server/server.php
+++ src/www/server/server.php
@@ -1,15 +1,28 @@
 <?php
-$prenom = $_POST["prenom"]
-$nom = $_POST["nom"]
-$niveau = $_POST["niveau"]
-$classe = $_POST["classe"]
-$instrument = $_POST["instrument"]
+$prenom = $_POST["prenom"];
+$nom2 = $_POST["nom"];
+$niveau = $_POST["niveau"];
+$classe = $_POST["classe"];
+$instrument = $_POST["Instrument"];
+$instrument2 = $_POST["Instrument2"];
+$jour = $_POST["jour"];
+$creneaux = $_POST["Creneaux"];
+$classe_t = $niveau + $classe;
+
+if ($instrument == "Autre"){
+    $instrument =  $instrument2;
+;}
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 	</head>
-		le test est <?php echo $classe; ?>
 	<body>
+		<?php echo $nom2 ; ?>
+		<?php echo $prenom ; ?>
+		<?php echo $classe_t ; ?>
+		<?php echo $instrument ; ?>
+		<?php echo $jour ; ?>
+		<?php echo $creneaux ; ?>
 	</body>
 </html>
