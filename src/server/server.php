6a82452bf9c2ab74071dcb5387acb7b81b05d918
Corentin <cdelpozo@passybuz.local>
2022-03-10 10:43:40 +0100

    test3

--- src/server/server.php
+++ src/server/server.php
@@ -1,10 +1,14 @@
 <?php
-$test = $_POST["reponse"]
+$prenom = $_POST["prenom"]
+$nom = $_POST["nom"]
+$classe = $_POST["classe"]
+$instrument = $_POST["instrument"]
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 	</head>
+		le test est <?php echo $classe; ?>
 	<body>
 	</body>
 </html>
