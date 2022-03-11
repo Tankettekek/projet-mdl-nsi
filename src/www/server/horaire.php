6d512948e5d466c47dc65a49fa7db0e4ae167085
LegionEcarlate <80052613+LegionEcarlate@users.noreply.github.com>
2022-03-11 17:52:08 +0100

    commit6

--- src/www/server/horaire.php
+++ src/www/server/horaire.php
@@ -11,7 +11,7 @@ $resp="";
 if ($q == "liste_jours"){
     $resp =  "<option value=''>Choisir une date</option>\n<option value='12/3/2022'>12/3/2022</option>\n<option value='19/3/2022'>19/3/2022</option>\n";
 } elseif ($q == "liste_creneaux" and !empty($d)){
-    $resp = "<option value=''>Choisir un creneau</option>\n<option value='12:00'>12:00</option>";
+    $resp = "<option value='12:00'>12:00</option>";
 };
 echo $resp
 ?>
