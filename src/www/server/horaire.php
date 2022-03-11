587c224da9db34dbdfd99478508bee5e2c660156
LegionEcarlate <80052613+LegionEcarlate@users.noreply.github.com>
2022-03-11 17:08:02 +0100

    jsp5

--- src/www/server/horaire.php
+++ src/www/server/horaire.php
@@ -3,9 +3,9 @@ $q = $_REQUEST["q"];
 $resp="";
 
 if ($q == "liste_jours"){
-    $resp =  "<option value='1'>12/3/2022</option>";
+    $resp =  "<option value='12/3/2022'>12/3/2022</option>";
 } elseif ($q == "liste_creneaux"){
-    $resp = "<option value='1'>12:00</option>";
+    $resp = "<option value='12:00'>12:00</option>";
 };
 echo $resp
 ?>
