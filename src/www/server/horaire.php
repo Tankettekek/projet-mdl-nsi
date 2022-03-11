55487b54f30e45a72ab7953aad258864d8a31e41
Tankettekek <alexandru.uzunov@gmail.com>
2022-03-11 17:35:50 +0100

    EUUUH JSP

--- src/www/server/horaire.php
+++ src/www/server/horaire.php
@@ -1,11 +1,17 @@
 <?php
 $q = $_REQUEST["q"];
+
+if( isset($_REQUEST["d"])){
+    $d = $_REQUEST["d"];
+}
+
+
 $resp="";
 
 if ($q == "liste_jours"){
-    $resp =  "<option value='12/3/2022'>12/3/2022</option>";
-} elseif ($q == "liste_creneaux"){
-    $resp = "<option value='12:00'>12:00</option>";
+    $resp =  "<option value=''>Choisir une date</option>\n<option value='12/3/2022'>12/3/2022</option>\n<option value='19/3/2022'>19/3/2022</option>\n";
+} elseif ($q == "liste_creneaux" and !empty($d)){
+    $resp = "<option value=''>Choisir un creneau</option>\n<option value='12:00'>12:00</option>";
 };
 echo $resp
 ?>
