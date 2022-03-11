5af0517a5d1c28e258b2d561ce16167d2d2e5c20
darkrentin <darkrentin@gmail.com>
2022-03-11 14:17:46 +0100

    jsp4

--- src/www/server/server.php
+++ src/www/server/server.php
@@ -7,22 +7,34 @@ $instrument = $_POST["Instrument"];
 $instrument2 = $_POST["Instrument2"];
 $jour = $_POST["jour"];
 $creneaux = $_POST["Creneaux"];
-$classe_t = $niveau + $classe;
+$classe_t = $niveau . " " . $classe;
 
 if ($instrument == "Autre"){
     $instrument =  $instrument2;
 ;}
 ?>
 <!DOCTYPE html>
+<html lang="en" data-theme="dark">
 <html>
 	<head>
+		<meta charset="UTF-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+		<style>
+        	body{
+            	background-image: url("../img/peakpx.jpg");
+        	}
+    	</style>
 	</head>
 	<body>
-		<?php echo $nom2 ; ?>
-		<?php echo $prenom ; ?>
-		<?php echo $classe_t ; ?>
-		<?php echo $instrument ; ?>
-		<?php echo $jour ; ?>
-		<?php echo $creneaux ; ?>
+		<main>
+			<?php echo"<h1>" . $nom2 . "</h1>" ; ?>
+			<h1><?php echo $prenom ; ?></h1>
+			<?php echo $classe_t ; ?>
+			<?php echo $instrument ; ?>
+			<?php echo $jour ; ?>
+			<?php echo $creneaux ; ?>
+		</main>
 	</body>
 </html>
