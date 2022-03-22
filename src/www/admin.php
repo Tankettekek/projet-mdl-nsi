4c89ee87bdb9ef07d3bd472e153d94f84a7b4930
Tankettekek <64431301+Tankettekek@users.noreply.github.com>
2022-03-22 17:19:34 +0100

    Bon bah le php ca marche

--- src/www/admin.php
+++ src/www/admin.php
@@ -1,25 +1,19 @@
-<!DOCTYPE html>
-<html lang="en" data-theme="dark">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page Admin</title>
-    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
-    <style>
-        body{
-            background-image: url("img/image3.png");
+<?php
+    $file = file("./server/info.csv");
+    $data = [];
+    foreach ($file as $line){
+        $csvline = str_getcsv($line, ";");
+        if (strtotime(str_replace("/", "-", $csvline[0])) > time()){
+            $data[] = $csvline;
         }
-        .container{
-            background-color: #11191f;
-            padding-left: 50px;
-            padding-right: 50px;
-        }
-    </style>
-</head>
-<body>
-    <main class="container">
-        <h1>admin</h1>
-    </main>
-</body>
-</html>
+    }
+    $pdata = [];
+    foreach ($data as $key => $arr){
+        $time = strtotime(str_replace("/", "-", $arr[0]) . " " . $arr[1]);
+        $slice = array_slice($arr, 2);
+        array_push($pdata, [$time]);
+        $pdata[$key] = array_merge($pdata[$key], $slice);
+    }
+    $colonnes = array_column($pdata, 0);
+    array_multisort($colonnes, SORT_ASC, $pdata);
+?>
