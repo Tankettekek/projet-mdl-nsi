6f1aacec9cf9ce94da415ef1e1591d5bc4165e58
Tankettekek <alexandru.uzunov@gmail.com>
2022-03-22 17:28:58 +0100

    Bon bah le php ca marche

--- src/www/admin.php
+++ src/www/admin.php
@@ -16,4 +16,43 @@
     }
     $colonnes = array_column($pdata, 0);
     array_multisort($colonnes, SORT_ASC, $pdata);
+    $out  = "";
+    $out .= "<table>";
+    foreach($pdata as $key => $element){
+        $out .= "<tr>";
+        foreach($element as $subkey => $subelement){
+            $out .= "<td>$subelement</td>";
+        }
+        $out .= "</tr>";
+    }
+    $out .= "</table>";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'administration</title>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+    <style>
+        body{
+            background-image: url("img/410283.jpg");
+        }
+        .container{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #11191f;
+            padding-left: 50px;
+            padding-right: 50px;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <main class="container">
+        <?php echo $out ?>
+    </main>
+</body>
+</html>
