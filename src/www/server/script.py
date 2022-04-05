35350a576ac9455b07c5fd0e328ee4a4b027916a
Darkrentin <79848980+Darkrentin@users.noreply.github.com>
2022-04-05 14:14:33 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaah2

--- src/www/server/script.py
+++ src/www/server/script.py
@@ -5,14 +5,14 @@ from collections import defaultdict
 
 def jeudis_suivants():
     #calcul de la date
-    test_date = datetime.datetime.now() 
+    test_date = datetime.datetime.now()
     weekday_idx = 3
-    
-    #calcul de la différence des jours 
+
+    #calcul de la différence des jours
     days_delta = weekday_idx - test_date.weekday()
     if days_delta <= 0:
         days_delta += 7
-    
+
     jeudi_prochain = test_date + datetime.timedelta(days_delta)
     jeudi_deuxieme_procain = jeudi_prochain +datetime.timedelta(7)
     jeudi_troi_prochain = jeudi_deuxieme_procain + datetime.timedelta(7)
@@ -23,21 +23,21 @@ def retour_jours(j1, j2, j3):
     columns = defaultdict(list)
     resp = ""
     with open('info.csv') as f:
-        reader = csv.DictReader(f, delimiter=";") 
+        reader = csv.DictReader(f, delimiter=";")
         for row in reader:
-            for (k,v) in row.items(): 
-                columns[k].append(v) 
-    if columns['Jour'].count(str(j1)) < 4:
-        resp = resp + "<option value='" + j1 +"'>" + j1 +"</option>"        
-    if columns['Jour'].count(str(j2)) < 4:
+            for (k,v) in row.items():
+                columns[k].append(v)
+    if columns['Jour'].count(str(j1)) < 5:
+        resp = resp + "<option value='" + j1 +"'>" + j1 +"</option>"
+    if columns['Jour'].count(str(j2)) < 5:
         resp = resp + "<option value='" + j2 +"'>" + j2 +"</option>"
-    if columns['Jour'].count(str(j3)) < 4:
+    if columns['Jour'].count(str(j3)) < 5:
         resp = resp + "<option value='" + j3 +"'>" + j3 +"</option>"
     return resp
 
 def retour_horaires(jour):
     resp = ""
-    liste_heures = ['11:00', '11:30', '12:00', '12:30']
+    liste_heures = ['11:00', '11:30', '12:00', '12:30', '13:00']
     with open('info.csv', 'r') as f:
         reader = csv.reader(f, delimiter=";")
         for row in reader:
@@ -46,7 +46,7 @@ def retour_horaires(jour):
                     if heure in row:
                         liste_heures.remove(heure)
     for heure in liste_heures:
-        resp = resp + "<option value='" + heure +"'>" + heure +"</option>" 
+        resp = resp + "<option value='" + heure +"'>" + heure +"</option>"
     return resp
 
 
